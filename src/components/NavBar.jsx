540d87e9dd643849e097c4f1973a3ad60236bfff
NovumChaldea <deusable@gmail.com>
2022-07-11 18:45:28 -0700

    added a test in style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,22 @@
-
-import React from 'react'
+import React, { Fragment } from 'react'
+import { VscSettingsGear } from 'react-icons/vsc';
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <Fragment>
+        <div className={styles.nav}>
+            <div className={styles.left}>
+                <VscSettingsGear className={styles.heartIcon}/>
+            </div>
+            <div className={styles.right}>
+                <h3>About Me</h3>
+                <h3>Projects</h3>
+                <h3>Contact!</h3>
+            </div>
+        </div>
+        
+    </Fragment> 
   )
 }
 
