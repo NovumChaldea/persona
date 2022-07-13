1e97eec7c18a6fdbe5dcfb6b239e86d6f667e743
NovumChaldea <deusable@gmail.com>
2022-07-12 22:27:25 -0700

    More style 2

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,8 +4,10 @@ import styles from '../styles/Banner.module.css';
 const Banner = () => {
   return (
     <div className={styles.background}>
-      <h1 className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti odio tenetur ipsum, praesentium temporibus natus in eligendi omnis quam tempora iure, aspernatur cupiditate inventore. Ipsa facere repellendus sunt tempora voluptate.</h1>
-      <h3 className={styles.littleText}>Little text here</h3>
+      <div className={styles.container}>
+        <h1 className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti odio tenetur ipsum, praesentium temporibus natus in eligendi omnis quam tempora iure, aspernatur cupiditate inventore. Ipsa facere repellendus sunt tempora voluptate.</h1>
+        <h3 className={styles.littleText}>Little text here</h3>
+      </div>
     </div>
   )
 }
