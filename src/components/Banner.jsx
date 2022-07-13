a714677845680b7f2ce4d567caf60eb7bf4251dd
NovumChaldea <deusable@gmail.com>
2022-07-12 22:12:50 -0700

    Text justify to banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,8 @@ import styles from '../styles/Banner.module.css';
 const Banner = () => {
   return (
     <div className={styles.background}>
-      asdasdas
+      <h1 className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti odio tenetur ipsum, praesentium temporibus natus in eligendi omnis quam tempora iure, aspernatur cupiditate inventore. Ipsa facere repellendus sunt tempora voluptate.</h1>
+      <h3 className={styles.littleText}>Little text here</h3>
     </div>
   )
 }
