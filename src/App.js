ee57489eb81bcac037a27da3e57abe0a863b211d
NovumChaldea <deusable@gmail.com>
2022-07-13 20:17:15 -0700

    Added carousel, contact and about section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React  from 'react';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
+import About  from './components/About';
+import Carousel from './components/Carousel';
+import Contact from './components/Contact';
 
 import styles from './styles/App.module.css'
 
@@ -11,7 +14,10 @@ const App = () => {
     <div className={styles.background}>
       <NavBar/>
       <Banner/>
+      <About/>
+      <Carousel/>
       <Footer/>
+      <Contact/>
     </div>
   )
 }
