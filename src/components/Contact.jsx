7e12a488d638b828d576b0bc2ecb3757b798738f
NovumChaldea <deusable@gmail.com>
2022-07-15 12:30:29 -0700

    start of the Contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from '../styles/Contact.module.css';
+import { BsGithub } from 'react-icons/bs';
 
 const Contact = () => {
   return (
     <div className={styles.structure}>
+      <h1>My plataforms</h1>
+      <div className={styles.icons}>
+        <BsGithub/>
+      </div>
     </div>
   )
 }
