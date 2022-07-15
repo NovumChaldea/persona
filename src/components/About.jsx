00dfc910ba4222128ee02bf6fa978b0bce74ea2b
NovumChaldea <deusable@gmail.com>
2022-07-14 19:57:53 -0700

    Changes in the About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,18 @@
 import React, { Fragment } from 'react';
+import styles from '../styles/About.module.css';
 
 const About = () => {
   return (
-    <Fragment>
-      <div>
-        <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo quae facere laudantium exercitationem est quibusdam perspiciatis quas eos voluptate, ex asperiores necessitatibus fuga. Quasi, a voluptatum maiores quos nisi soluta.</h3>
-        <img></img>
-      </div>
-      <div>
-        <img></img>
-        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, saepe id! Eos, possimus quisquam quibusdam incidunt quae facilis consequatur assumenda saepe delectus, exercitationem, optio necessitatibus suscipit fugit ab cum! Maxime.</h3>
+    <Fragment >
+      <div className={styles.box}>
+        <div className={styles.container}>
+          <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo quae facere laudantium exercitationem est quibusdam perspiciatis quas eos voluptate, ex asperiores necessitatibus fuga. Quasi, a voluptatum maiores quos nisi soluta.</h3>
+          <img></img>
+        </div>
+        <div className={styles.container}>
+          <img></img>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, saepe id! Eos, possimus quisquam quibusdam incidunt quae facilis consequatur assumenda saepe delectus, exercitationem, optio necessitatibus suscipit fugit ab cum! Maxime.</h3>
+        </div>
       </div>
     </Fragment>
   );
