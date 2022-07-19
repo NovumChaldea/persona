a5e37dcdce66d8c77f6ee794ba7ef5af0d91c95c
NovumChaldea <deusable@gmail.com>
2022-07-18 21:57:49 -0700

    Added comments in the Index section

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Carousel from './components/Carousel';
 import Contact from './components/Contact';
 
 import styles from './styles/App.module.css';
-
+//It's important to create a container to contrast the background from the sections
 
 const App = () => {
   return (
