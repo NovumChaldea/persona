cc448fc9a9cc207678c1eff71c741b6569c1149d
NovumChaldea <deusable@gmail.com>
2022-07-19 22:32:55 -0700

    Added dummy images to the sideshow component

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import styles from '../styles/Slideshow.module.css';
-import dummy from '../assets/dummy.jpg';
+import img1 from '../assets/1.jpg';
+import FlechaIzquierda from '../assets/iconmonstr-angel-left-thin.svg';
+import FlechaDerecha from '../assets/iconmonstr-angel-right-thin.svg'
 
 const Slideshow = () => {
   return (
     <div>
         <div>
             <a href="https://www.google.com">
-                <img src={dummy} alt="" className={styles.images}/>
+                <img src={img1} alt="" />
             </a>
         </div>
-        <div>Controls</div>
+        <div>
+            <button>
+                <img src={FlechaIzquierda} alt=""/>
+            </button>
+            <button>
+                <img src={FlechaDerecha} alt=""/>
+            </button>
+        </div>
     </div>
   )
 }
