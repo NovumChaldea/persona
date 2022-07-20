930ddf0cd26a262d8e88c559bb036178fa581838
NovumChaldea <deusable@gmail.com>
2022-07-19 21:49:17 -0700

    First structure of the slideshow

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import styles from '../styles/Carousel.module.css';
+import Slideshow from './Slideshow';
 //The carousel need to be reactive and in a loop
 //The carousel´s section must be wide and centric
 
@@ -8,7 +9,7 @@ const Carousel = () => {
     <Fragment>
       <div className={styles.container}>
         <h1 className={styles.title}>Projects</h1>
-        <div className={styles.carouselContainer}>Here be a Carousel</div>
+        <Slideshow/>
       </div>
     </Fragment>
   )
