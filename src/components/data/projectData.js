323caee56d4f3a1de5baa21f0b66ac0ddffc6450
NovumChaldea <deusable@gmail.com>
2022-07-20 20:07:23 -0700

    Added first structure of the project data structure

--- src/components/data/projectData.js
+++ src/components/data/projectData.js
@@ -1,3 +1,18 @@
-const projectsData = [
+const projectsData = {
+    project1 :{
+        projectImg :'dummy img url',
+        projectText: 'dummy text',
+        projectLinks :{
+            github:'',
+            live: ''
+        }
+    },
+    project2 :{
+        projectImg :'dummy img url',
+        projectText: 'dummy text',
+        projectLinks :{
+            github:'',
+            live: ''
+        }
     
-]
+}
